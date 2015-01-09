db21e0871d4edfa5eab202af696a3e927ada7309
Griffin Caprio <me@griffincaprio.com>
2015-01-08 19:23:57 -0600

    Adding context processor to include settings.

---
+++ questionnaire/context_processors/settings.py
@@ -0,0 +1,23 @@
+from django.conf import settings
+
+
+def load_settings(request):
+    """
+    A template context processor that adds the Questionnaire
+     Settings to the template context.
+    """
+    try:
+        use_session = settings.QUESTIONNAIRE_USE_SESSION
+    except AttributeError:
+        use_session = False
+
+    try:
+        debug_questionnaire = settings.QUESTIONNAIRE_DEBUG
+    except AttributeError:
+        debug_questionnaire = False
+
+    context = {
+        'debug_questionnaire': debug_questionnaire,
+        'use_sesison': use_session
+    }
+    return context
